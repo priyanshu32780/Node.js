2b5f82ae08c3d45281efa4934de2d72460f458a1
priyanshu jha <jhapriyanshu546@gmail.com>
2026-02-11 07:09:47 -0800

    Add files via upload

---
+++ video 2/main.js
@@ -0,0 +1,9 @@
+// import {a,b,d} from "./mymodule.js"
+// console.log(a,b,d)
+
+
+import priyanshu from "./mymodule.js"
+console.log(priyanshu)
+
+// const a = require("./mymodule2.js")
+// console.log(a)
